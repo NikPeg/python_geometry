46e1cd6608536a47ea53ffa5c9647eef948e1212
nspeganov <nspeganov@yandex-team.ru>
2021-10-31 00:10:38 +0300

    Add Point class

---
+++ Point.py
@@ -0,0 +1,18 @@
+import math
+import settings
+
+
+class Point:
+    x = 0
+    y = 0
+
+    def __init__(self, _x, _y):
+        self.x = _x
+        self.y = _y
+
+    @staticmethod
+    def distance(a, b):
+        return math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y))
+
+    def print(self):
+        settings.fout.write(f"({self.x}, {self.y})")
