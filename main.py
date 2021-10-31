49ed1ddad717519363f4bc6f58253898e21d746a
nspeganov <nspeganov@yandex-team.ru>
2021-10-31 11:44:40 +0300

    Delete time

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import Container
 import settings
 import sys
-import time
 
 
-start = time.time()
 settings.fin = open(sys.argv[1], "r")
 settings.fout = open(sys.argv[2], "w")
 c = Container.Container()
@@ -13,5 +11,3 @@ c.sort()
 c.print()
 settings.fin.close()
 settings.fout.close()
-end = time.time()
-print((end - start) * 1000)
