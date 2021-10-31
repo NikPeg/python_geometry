6d3cc28646d5c895e77df199cfe6c7a67aa14264
nspeganov <nspeganov@yandex-team.ru>
2021-10-31 11:44:00 +0300

    Normal report

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import Container
 import settings
 import sys
+import time
 
 
+start = time.time()
 settings.fin = open(sys.argv[1], "r")
 settings.fout = open(sys.argv[2], "w")
 c = Container.Container()
@@ -11,3 +13,5 @@ c.sort()
 c.print()
 settings.fin.close()
 settings.fout.close()
+end = time.time()
+print((end - start) * 1000)
